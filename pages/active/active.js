c96d55cc0ae0494be823f1f514c9493b65be8378
huanbi <1404735131@qq.com>
2020-07-03 22:18:12 +0800

    添加了向上和详情页初始
    可以传参数但是显示不出数据，后面再改，优化了一下页面结构

--- pages/active/active.js
+++ pages/active/active.js
@@ -1,18 +1,56 @@
 // pages/active/active.js
 const App = getApp();
 Page({
+  getnews:function(event){
+    // console.log("outer view bindtap")
+    console.log(event);
+    var idx = event.currentTarget.dataset.newsid;
+    console.log(idx);    // 新闻url 
+     wx.navigateTo({
+      // url: "../view/view?id=" + idx
+     });
+  },
+
+  // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    console.log(e)
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+
+  //回到顶部
+  goTop: function (e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
   /**
    * 页面的初始数据
    */
+  
   data: {
       ids:1,
     news1:[{
       id:1,
       //idm:1,
-      title:'第一条活动',
+      title:'迷你马拉松',
       imgurl:'img/1.jpg',
-      author:'张三',
-      place:'南山',
+      author:'三翼工作室',
+      place:'一田活动中心',
       date:'2019-6-27 20:08'
     },
     {
@@ -41,11 +79,38 @@ Page({
       author: '王三',
       place:'北山',
       date: '2019-6-25 10:10'
-    }
+    },
+    {
+      id:5,
+      //idm:1,
+      title: '第二条新闻',
+      imgurl: 'img/4.jpg',
+      author: '李四',
+      place:'二田运动',
+      date: '2019-6-25 10:10'
+    },
+    {
+      id:6,
+      //idm:1,
+      title: '第二条新闻',
+      imgurl: 'img/4.jpg',
+      author: '李四',
+      place:'二田运动',
+      date: '2019-6-25 10:10'
+    },
+    {
+      id:7,
+      //idm:1,
+      title: '第二条新闻',
+      imgurl: 'img/4.jpg',
+      author: '李四',
+      place:'二田运动',
+      date: '2019-6-25 10:10'
+    },
   ],
 
   news2:[{
-    id:1,
+    id:5,
     //idm:1,
     title:'文艺',
     imgurl:'img/8.jpg',
@@ -54,7 +119,7 @@ Page({
     date:'2019-6-27 20:08'
   },
   {
-    id:2,
+    id:6,
     //idm:1,
     title: '第二条新闻',
     imgurl: 'img/22.jpg',
@@ -63,7 +128,7 @@ Page({
     date: '2019-6-25 10:10'
   },
   {
-    id:3,
+    id:7,
     //idm:1,
     title: '第3条新闻',
     imgurl: 'img/28.jpg',
@@ -72,7 +137,7 @@ Page({
     date: '2019-6-25 10:10'
   },
   {
-    id:4,
+    id:8,
    // idm:2,
     title: '第4条新闻',
     imgurl: 'img/4.jpg',
@@ -83,7 +148,7 @@ Page({
 ],
 
 news3:[{
-  id:1,
+  id:9,
   //idm:1,
   title:'竞赛',
   imgurl:'img/4.jpg',
@@ -92,7 +157,7 @@ news3:[{
   date:'2019-6-27 20:08'
 },
 {
-  id:2,
+  id:10,
   //idm:1,
   title: '第二条新闻',
   imgurl: 'img/28.jpg',
@@ -101,7 +166,7 @@ news3:[{
   date: '2019-6-25 10:10'
 },
 {
-  id:3,
+  id:11,
   //idm:1,
   title: '第3条新闻',
   imgurl: 'img/8.jpg',
@@ -110,7 +175,7 @@ news3:[{
   date: '2019-6-25 10:10'
 },
 {
-  id:4,
+  id:12,
  // idm:2,
   title: '第4条新闻',
   imgurl: 'img/4.jpg',
@@ -184,5 +249,8 @@ news3:[{
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  
+  
+  
 })
