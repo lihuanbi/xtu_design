ce09a4023cba6d019bcf1203531f4f923c0ddc49
huanbi <1404735131@qq.com>
2020-07-03 11:03:18 +0800

    新增了选项卡导航侧栏
    优化了一下图片方向和新增了地点项目

--- pages/active/active.js
+++ pages/active/active.js
@@ -5,37 +5,128 @@ Page({
    * 页面的初始数据
    */
   data: {
-    news:[{
+      ids:1,
+    news1:[{
       id:1,
-      title:'第一条新闻',
+      //idm:1,
+      title:'第一条活动',
       imgurl:'img/1.jpg',
       author:'张三',
+      place:'南山',
       date:'2019-6-27 20:08'
     },
     {
       id:2,
+      //idm:1,
       title: '第二条新闻',
       imgurl: 'img/4.jpg',
       author: '李四',
+      place:'二田运动',
       date: '2019-6-25 10:10'
     },
     {
       id:3,
+      //idm:1,
       title: '第3条新闻',
       imgurl: 'img/1.jpg',
       author: '王三',
+      place:'北山',
       date: '2019-6-25 10:10'
     },
     {
       id:4,
+     // idm:2,
       title: '第4条新闻',
       imgurl: 'img/4.jpg',
       author: '王三',
+      place:'北山',
       date: '2019-6-25 10:10'
     }
-  ]
-  },
+  ],
 
+  news2:[{
+    id:1,
+    //idm:1,
+    title:'文艺',
+    imgurl:'img/8.jpg',
+    author:'张三',
+    place:'南山',
+    date:'2019-6-27 20:08'
+  },
+  {
+    id:2,
+    //idm:1,
+    title: '第二条新闻',
+    imgurl: 'img/22.jpg',
+    author: '李四',
+    place:'二田运动',
+    date: '2019-6-25 10:10'
+  },
+  {
+    id:3,
+    //idm:1,
+    title: '第3条新闻',
+    imgurl: 'img/28.jpg',
+    author: '王三',
+    place:'北山',
+    date: '2019-6-25 10:10'
+  },
+  {
+    id:4,
+   // idm:2,
+    title: '第4条新闻',
+    imgurl: 'img/4.jpg',
+    author: '王三',
+    place:'北山',
+    date: '2019-6-25 10:10'
+  }
+],
+
+news3:[{
+  id:1,
+  //idm:1,
+  title:'竞赛',
+  imgurl:'img/4.jpg',
+  author:'张三',
+  place:'南山',
+  date:'2019-6-27 20:08'
+},
+{
+  id:2,
+  //idm:1,
+  title: '第二条新闻',
+  imgurl: 'img/28.jpg',
+  author: '李四',
+  place:'二田运动',
+  date: '2019-6-25 10:10'
+},
+{
+  id:3,
+  //idm:1,
+  title: '第3条新闻',
+  imgurl: 'img/8.jpg',
+  author: '王三',
+  place:'北山',
+  date: '2019-6-25 10:10'
+},
+{
+  id:4,
+ // idm:2,
+  title: '第4条新闻',
+  imgurl: 'img/4.jpg',
+  author: '王三',
+  place:'北山',
+  date: '2019-6-25 10:10'
+}
+]
+  },
+  switchRightTab:function(e){
+    let id = e.target.dataset.id;
+    console.log(id);
+    this.setData({
+      ids: id
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
